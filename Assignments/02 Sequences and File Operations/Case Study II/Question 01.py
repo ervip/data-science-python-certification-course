732a4b54ba8d3ba9bf86230704ffaa54807b9935
Vishv Patel <139690991+rarevp@users.noreply.github.com>
2023-08-04 19:45:51 +0530

    assignment: Module 2 > Case Study II > Question 1 - related to set

---
+++ Assignments/02 Sequences and File Operations/Case Study II/Question 01.py
@@ -0,0 +1,7 @@
+"""
+What is the output of the following code?
+Answer: As we are converting list to set, duplicates will be absent in set and answer will be 4.
+"""
+
+nums = set([1,1,2,3,3,3,4,4])
+print(len(nums))
