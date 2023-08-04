3b36a3e6eee7ab62493928c733a18a2dcadcb789
Vishv Patel <139690991+rarevp@users.noreply.github.com>
2023-08-04 19:50:37 +0530

    assignment: Module 2 > Case Study II > Question 4 - loop index related

---
+++ Assignments/02 Sequences and File Operations/Case Study II/Question 04.py
@@ -0,0 +1,8 @@
+"""
+Write a for loop that prints all elements of a list and their position in the list.
+a = [4,7,3,2,5,9]
+"""
+
+a = [4,7,3,2,5,9]
+for index, i in enumerate(a):
+    print(f"{i} at index {index}")
